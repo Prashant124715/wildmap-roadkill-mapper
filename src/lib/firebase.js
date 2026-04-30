5961ae3de6c3a6eed82aaddf5676bd91d9ee2c6c
prashant <ghugeprashant44@gmail.com>
2026-04-30 20:31:14 +0530

    hide api key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWMV0ZdNdvnCmRo56RoR9e92_niyvo5jM",
-  authDomain: "wildlife-hotspot-mapper.firebaseapp.com",
-  projectId: "wildlife-hotspot-mapper",
-  storageBucket: "wildlife-hotspot-mapper.firebasestorage.app",
-  messagingSenderId: "923736940746",
-  appId: "1:923736940746:web:1f0a6b79bef10c08f39433",
-  measurementId: "G-KN9YWCH88G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
